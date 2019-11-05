adbb1673b7acafc04c76e072a843a35b6a534f46
Charu <charu1997@gmail.com>
2019-11-04 21:27:43 -0800

    added postgress connection

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const { Pool } = require('pg');
+const { Pool } = require('pg');
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
@@ -7,10 +7,23 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true
-// });
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true  
+});
